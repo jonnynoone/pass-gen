f15759a1694f0629eac7d6f73f2b59cc7f6e9b46
Jonny Noone <jonny.f.noone@gmail.com>
2019-05-11 22:19:32 +0100

    initial commit; static page

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Password Generator</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet">
+    <!-- Custom Styles -->
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+
+<body>
+    <form class="form-signin">
+        <div class="text-center mb-4">
+            <i class="fas fa-key"></i>
+
+            <h1 class="h3 mb-3 font-weight-normal">Password Generator</h1>
+
+            <!--<p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element.</p>-->
+        </div>
+
+        <!--
+        <div class="form-label-group">
+            <input type="text" id="inputName" class="form-control" placeholder="Name" required autofocus>
+            <label for="inputName">Name</label>
+        </div>
+    	-->
+
+    	<!--
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="include-numbers"> Include Numbers?
+            </label>
+        </div>
+    	-->
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Generate!</button>
+    </form>
+</body>
+
+</html>
