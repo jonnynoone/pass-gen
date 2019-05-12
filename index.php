034cd41998bf4635e86e95f6cc7dbd2cab34b726
Jonny Noone <jonny.f.noone@gmail.com>
2019-05-12 03:08:00 +0100

    added basic lowercase 12 char generator; no options

--- index.php
+++ index.php
@@ -1,46 +1,75 @@
+<?php
+
+$password = "";
+
+$lowercase = array(
+	'a','b','c','d','e','f','g','h','i','j','k','l','m',
+	'n','o','p','q','r','s','t','u','v','w','x','y','z'
+);
+
+if (isset($_POST['submit'])) {
+	for ($i = 0; $i < 12; $i++) {
+		$random = rand(0, 25);
+		$password .= $lowercase[$random];
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Password Generator</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Password Generator</title>
 
-    <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet">
-    <!-- Custom Styles -->
-    <link href="css/custom.css" rel="stylesheet">
+	<!-- Bootstrap CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Font Awesome -->
+	<link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet">
+	<!-- Custom Styles -->
+	<link href="css/custom.css" rel="stylesheet">
 </head>
 
 <body>
-    <form class="form-signin">
-        <div class="text-center mb-4">
-            <i class="fas fa-key"></i>
+	<form class="form-signin" action="index.php" method="post">
+		<div class="text-center mb-4">
+			<i class="fas fa-key"></i>
 
-            <h1 class="h3 mb-3 font-weight-normal">Password Generator</h1>
+			<h1 class="h3 mb-3 font-weight-normal">Password Generator</h1>
 
-            <!--<p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element.</p>-->
-        </div>
+			<p><?php echo $password; ?></p>
+		</div>
 
-        <!--
-        <div class="form-label-group">
-            <input type="text" id="inputName" class="form-control" placeholder="Name" required autofocus>
-            <label for="inputName">Name</label>
-        </div>
-    	-->
+		<!--
+		<div class="form-label-group">
+			<input type="text" id="inputName" class="form-control" placeholder="Name" required autofocus>
+			<label for="inputName">Name</label>
+		</div>
+		-->
 
-    	<!--
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="include-numbers"> Include Numbers?
-            </label>
-        </div>
-    	-->
+		<!--
+		<div class="checkbox mb-3">
+			<label>
+				<input type="checkbox" value="include-numbers"> Include symbols?
+			</label>
+		</div>
+		-->
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Generate!</button>
-    </form>
+		<!--
+		<div class="form-check">
+			<input type="radio" name="charSelect" checked>
+			<label for="materialChecked">12 Characters</label>
+			<input type="radio" name="charSelect">
+			<label for="materialUnchecked">16 Characters</label>
+			<input type="radio" name="charSelect">
+			<label for="materialUnchecked">20 Characters</label>
+		</div>
+		-->
+
+		<button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Generate!</button>
+	</form>
 </body>
 
 </html>
