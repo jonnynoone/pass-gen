528075a9d0c59d826fe8a9c0537c64d358384f95
Jonny Noone <jonny.f.noone@gmail.com>
2019-05-13 21:50:25 +0100

    added option to choose amount of numbers generated

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php 
 
 if (isset($_POST['submit'])) {
-	$mobile = '07';
+    $numbers = array();
+    $amount = intval($_POST['amount']);
 
-	for ($i = 1; $i <= 9; $i++) {
-		$random = rand(0, 9);
-		$mobile .= $random;
-	}
+    for ($i = 0; $i < $amount; $i++) {
+        $mobile = '07';
+
+        for ($j = 1; $j <= 9; $j++) {
+            $random = rand(0, 9);
+            $mobile .= $random;
+        }
+
+        array_push($numbers, $mobile);
+    }
+
+    $number_list = implode(", \n", $numbers);
+} else {
+    $amount = 1;
 }
 
 ?>
@@ -35,18 +46,41 @@ if (isset($_POST['submit'])) {
             <h1 class="h3 mb-3 font-weight-normal">Mobile Number Generator</h1>
         </div>
 
+        <div class="form-label-group">
+            <select class="form-control" name="amount">
+                <option value="1" <?php echo ($amount == 1) ? "selected='true'" : ""; ?>>1</option>
+                <option value="5" <?php echo ($amount == 5) ? "selected='true'" : ""; ?>>5</option>
+                <option value="10" <?php echo ($amount == 10) ? "selected='true'" : ""; ?>>10</option>
+                <option value="100" <?php echo ($amount == 100) ? "selected='true'" : ""; ?>>100</option>
+            </select>
+        </div>
+
         <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Generate!</button>
 
-        <?php if (isset($_POST['submit'])) {
-        	echo "
-	        <div class=\"form-label-group\">
-	            <input type=\"text\" id=\"mobNumber\" class=\"form-control\" value=\"$mobile\">
-	            <label for=\"mobNumber\">Mobile Number</label>
-	        </div>
+        <?php 
 
-        	<button class=\"btn btn-lg btn-primary btn-block\" type=\"button\" id=\"copyNum\">Copy</button>
-        	";
-    	} ?>
+        if (isset($_POST['submit'])) {
+            if ($amount == 1) {
+                echo "
+                <div class=\"form-label-group\">
+                    <input type=\"text\" id=\"mobNumber\" class=\"form-control\" value=\"$mobile\">
+                    <label for=\"mobNumber\">Mobile Number</label>
+                </div>
+                ";
+            } else {
+                echo "
+                <div class=\"form-label-group\">
+                    <textarea id=\"mobNumber\" class=\"form-control\" rows=\"6\">$number_list</textarea>
+                </div>
+                ";
+            }
+
+            echo "
+            <button class=\"btn btn-lg btn-primary btn-block\" type=\"button\" id=\"copyNum\">Copy</button>
+            ";
+    	}
+
+        ?>
 
     	<?php if (isset($_POST['submit'])) { ?>
     		<script>
@@ -54,6 +88,7 @@ if (isset($_POST['submit'])) {
 	    			var mobNo = document.querySelector("#mobNumber");
 	    			mobNo.select();
 	    			document.execCommand('copy');
+                    document.getSelection().removeAllRanges();
     			};
 
     			var button = document.querySelector("#copyNum");
